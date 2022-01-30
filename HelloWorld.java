213235b49a8f15c6d609904d279ce1f469541e47
Colbylee Mincey <cmincey@stu.jsu.edu>
2022-01-30 16:45:05 -0600

    Initial Commit

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main (String[] args) {
+        Class c1 = new Class1();
+        Class c2 = new Class();
+        System.out.println("Hello World!");
+    }
+    private void f1() {
+        //Placeholder method (v1.0)
+    }
+}
